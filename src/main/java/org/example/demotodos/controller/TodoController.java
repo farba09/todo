736d5c61886f9abe58fd0eeb6134f44dbf5839e5
farba09 <pisco7155@gmail.com>
2025-07-18 14:16:42 +0200

    ajout de la liste de todo, mais rien en service et interfaces

--- src/main/java/org/example/demotodos/controller/TodoController.java
+++ src/main/java/org/example/demotodos/controller/TodoController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/api")
 public class TodoController {
@@ -14,5 +17,16 @@ public class TodoController {
     public Todo getTodo() {
         Todo listodo = Todo.builder().id(1).name("lire").description("livre sur la programmation").done(true).build();
         return listodo;
+    }
+
+    @RequestMapping("/liste-todos")
+    @ResponseBody
+    public List<Todo> getListeTodos() {
+        List<Todo> todos = Arrays.asList(
+                Todo.builder().id(1).name("lire").description("livre sur la programmation").done(true).build(),
+                Todo.builder().id(2).name("coder").description("faire un exo Spring Boot").done(false).build(),
+                Todo.builder().id(3).name("courir").description("faire un footing").done(false).build()
+        );
+        return todos;
     }
 }
